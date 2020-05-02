ee1fdef84c83a4e446e38071be2ee02dc66743df
Anupama Unnikrishnan <anu@Anupamas-MacBook-Pro.local>
2020-05-02 17:12:56 +0100

    website

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,24 @@ siteMetadata: {
 title: `Anu Unnikrishnan`,
 },
 plugins: [
+    { //from here (this goes below)
+        resolve: `gatsby-source-filesystem`,
+        options: {
+        path: `${__dirname}/src/pages`,
+        name: "media"
+        },
+        }, //to here (this goes below)
           `gatsby-plugin-sharp`, `gatsby-transformer-sharp`, //added this second one
           {
           resolve: `gatsby-transformer-remark`,
           options: {
           plugins: [
+            { //delete from here
+                resolve: `gatsby-remark-relative-images`,
+                options: {
+                  name: "media" // Must match the source name ^
+                }
+              }, //to here 
                     {
                     resolve: `gatsby-remark-images`,
                     options: {
@@ -17,12 +30,7 @@ plugins: [
                     ],
           },
           },
-          {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-          path: `${__dirname}/src/pages`,
-          },
-          },
+          //here goes gatsby-source-filesystem
           `gatsby-plugin-emotion`,
           {
           resolve: `gatsby-plugin-typography`,
